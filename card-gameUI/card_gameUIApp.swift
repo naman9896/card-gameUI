6f6e089d938e8aa99dacc1583e7bf095df7e068b
Naman <namanmattu36166@gmail.com>
2021-11-05 18:48:31 +0530

    Initial Commit

---
+++ card-gameUI/card_gameUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  card_gameUIApp.swift
+//  card-gameUI
+//
+//  Created by Naman on 05/11/21.
+//
+
+import SwiftUI
+
+@main
+struct card_gameUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
